c2d02bca078e56199e5d92c7e6fb11d750abe102
Ritesh <ritesh@aroracomfortechs.com>
2017-12-04 12:34:50 +0530

    login invalid message in web, scroll changes for ionic app

--- codeignitercode/application/views/admin/login_form.php
+++ codeignitercode/application/views/admin/login_form.php
@@ -26,6 +26,7 @@
             <div class="logo text-uppercase"><img src="<?php echo admin_img('logo_admin.png'); ?>"></div>
             <div class="logo text-uppercase"><span>Admin</span><strong class="text-primary">Login</strong></div>
             <form action="<?=admin_url('login/process');?>" role="form" method="post">
+			  <div class="alert alert-danger"><?php echo $this->session->flashdata('message'); ?></div>
               <div class="form-group">
                 <input type="text" class="form-control" placeholder="Username" name="username">
               </div>
